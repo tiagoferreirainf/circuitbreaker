ffa8e4a3a6f78498c4d9f3c543e4a62c21127f84
Tiago Ferreira <tiago.ferreira.inf@gmail.com>
2022-11-09 10:32:28 +0000

    update circuitbreaker

--- circuitbreaker/src/main/java/org/tf/training/circuitbreaker/controller/CircuitBreakerController.java
+++ circuitbreaker/src/main/java/org/tf/training/circuitbreaker/controller/CircuitBreakerController.java
@@ -2,26 +2,29 @@ package org.tf.training.circuitbreaker.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.tf.training.circuitbreaker.model.Response;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 @RestController
 @RequestMapping("/cb")
 public class CircuitBreakerController {
 
-    private final RestTemplate restTemplate;
-
+    private static final String CIRCUIT_BREAKER_FALLBACK = "circuitBreakerFallback";
+    private static final String RETRY_FALLBACK = "retryFallback";
     private static final String BASE_URL = "http://localhost:8081/service";
-
     private static final String CIRCUIT_NAME = "serviceA";
+    private final AtomicInteger numberOfCalls = new AtomicInteger(0);
 
-    AtomicInteger numberOfCalls = new AtomicInteger(0);
+    private final RestTemplate restTemplate;
 
     @Autowired
     public CircuitBreakerController(RestTemplate restTemplate) {
@@ -29,21 +32,24 @@ public class CircuitBreakerController {
     }
 
     @GetMapping
-    @CircuitBreaker(name = CIRCUIT_NAME, fallbackMethod = "circuitBreakerFallback")
+    @CircuitBreaker(name = CIRCUIT_NAME, fallbackMethod = CIRCUIT_BREAKER_FALLBACK)
     @Retry(name = CIRCUIT_NAME)
     //@RateLimiter(name = CIRCUIT_NAME)
-    public String testController(){
-        System.out.println(numberOfCalls.addAndGet(1) + " Making a request to " + BASE_URL + " at :" + LocalDateTime.now());
+    public String testController() {
+        log.info(numberOfCalls.addAndGet(1) + " Making a request to " + BASE_URL + " at :" + LocalDateTime.now());
+        restTemplate.getForObject(BASE_URL, String.class);
         return restTemplate.getForObject(BASE_URL, String.class);
     }
 
-    private String circuitBreakerFallback(Exception e){
-        System.out.println(" CircuitBreaker Fallback at :" + LocalDateTime.now());
+    @SuppressWarnings("unused")
+    private String circuitBreakerFallback(Exception e) {
+        log.info(" CircuitBreaker Fallback at :" + LocalDateTime.now());
         return "This is the fallback for circuitBreaker";
     }
 
-    private String retryFallback(Exception e){
-        System.out.println(" Retry Fallback at :" + LocalDateTime.now());
+    @SuppressWarnings("unused")
+    private String retryFallback(Exception e) {
+        log.info(" Retry Fallback at :" + LocalDateTime.now());
         return "This is the fallback for retry";
     }
 
